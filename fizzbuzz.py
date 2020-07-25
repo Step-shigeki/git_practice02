755576de7446452fe26f908d8c8444123c0d4b73
furukawashigeki <full2zinfi@gmail.com>
2020-07-25 21:32:50 +0900

    3,5,15の倍数以外でそのまま数字を実装

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,9 +8,12 @@ def fizzbuzz_convert(number):
     if number % 5 == 0:
         return 'Buzz'
 
+    return str(number)
+
 
 assert fizzbuzz_convert(3) == 'Fizz'
 
 assert fizzbuzz_convert(5) == 'Buzz'
 
 assert fizzbuzz_convert(15) == 'FizzBuzz'
+assert fizzbuzz_convert(7) == '7'
