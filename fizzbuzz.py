f16356f7c246330daaae02bb44597e80726bd440
furukawashigeki <full2zinfi@gmail.com>
2020-07-25 21:28:37 +0900

    5の倍数のfizzbuzzに実装

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,5 +2,10 @@ def fizzbuzz_convert(number):
     if number % 3 == 0:
         return 'Fizz'
 
+    if number % 5 == 0:
+        return 'Buzz'
+
 
 assert fizzbuzz_convert(3) == 'Fizz'
+
+assert fizzbuzz_convert(5) == 'Buzz'
