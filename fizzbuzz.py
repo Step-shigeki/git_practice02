e622e8c0bff162f553cb545f14b224fb721fb410
furukawashigeki <full2zinfi@gmail.com>
2020-07-25 21:30:12 +0900

    15の倍数のfizzbuzzに実装

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,7 @@
 def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        return 'FizzBuzz'
+
     if number % 3 == 0:
         return 'Fizz'
 
@@ -9,3 +12,5 @@ def fizzbuzz_convert(number):
 assert fizzbuzz_convert(3) == 'Fizz'
 
 assert fizzbuzz_convert(5) == 'Buzz'
+
+assert fizzbuzz_convert(15) == 'FizzBuzz'
